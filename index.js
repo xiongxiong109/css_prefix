86b640e4eac037b5a0e30457dfd9e874825d4ae9
xiongxiong109 <444122662@qq.com>
2016-10-09 21:33:19 +0800

    css_prefix

--- index.js
+++ index.js
@@ -4,42 +4,60 @@ const autoprefixer = require('autoprefixer');
 const postcss      = require('postcss');
 const fs           = require('fs');
 const async        = require('async');
+const throttle     = require('lodash/throttle');
 
 let cleaner  = postcss([ autoprefixer({ add: false, browsers: [] }) ]);
 let prefixer = postcss([ autoprefixer ]);
 
-// 读取整个css目录下的文件
+let _throPrefixFile = throttle(prefixCssFile, 500);
+
 var cssPath = `${__dirname}/css`;
-fs.readdir(cssPath, (err, filenames) => {
 
-	if (err) throw err;
-	async.each(filenames, (file) => {
+readCompileCssDir();
 
-		fs.readFile(`${cssPath}/${file}`, (err, data) => {
-			console.log(data.toString());
+// 监听文件变化
+fs.watch(cssPath, (ev, file) => {
+	console.log(`${file} has been changed, compiling...`);
+	if (/\.css$/.test(file)) {
+		_throPrefixFile(file);
+	}
+
+})
+
+// 读取整个css目录下的文件并编译
+function readCompileCssDir() {
+	fs.readdir(cssPath, (err, filenames) => {
+
+		if (err) throw err;
+		async.each(filenames, (file) => {
+
+			if (/\.css$/.test(file)) {
+				prefixCssFile(file);
+			}
+
+		});
+
+	});
+}
+
+function prefixCssFile(css) {
+
+	fs.readFile(`${cssPath}/${css}`, (err, data) => {
+
+		let cssStr = data.toString();
+		cleaner.process(cssStr).then(function (cleaned) {
+		    return prefixer.process(cleaned.css)
+		}).then(function (result) {
+		    savePrefixerCss(result.css, css);
 		});
 
 	});
 
-});
+}
 
-// fs.readFile('./css/style.css', (err, data) => {
-
-// 	if (err) throw err;
-
-// 	let cssStr = data.toString();
-
-// 	cleaner.process(cssStr).then(function (cleaned) {
-// 	    return prefixer.process(cleaned.css)
-// 	}).then(function (result) {
-// 	    savePrefixerCss(result.css);
-// 	});
-
-// });
-
-// function savePrefixerCss(css) {
-// 	fs.writeFile('./dest/style.css', css, (err) => {
-// 		if (err) throw err;
-// 		console.log('file write successed');
-// 	});
-// }
+function savePrefixerCss(css, cssName) {
+	fs.writeFile(`${__dirname}/dest/${cssName}`, css, (err) => {
+		if (err) throw err;
+		console.log(`${__dirname}/dest/${cssName} write successed`);
+	});
+}
