2a47647e0cd9b877ab45faba828dd4614cd51250
xiongxiong109 <444122662@qq.com>
2016-10-09 19:06:32 +0800

    update files

--- index.js
+++ index.js
@@ -2,28 +2,44 @@
 
 const autoprefixer = require('autoprefixer');
 const postcss      = require('postcss');
-const fs = require('fs');
+const fs           = require('fs');
+const async        = require('async');
 
-var cleaner  = postcss([ autoprefixer({ add: false, browsers: [] }) ]);
-var prefixer = postcss([ autoprefixer ]);
+let cleaner  = postcss([ autoprefixer({ add: false, browsers: [] }) ]);
+let prefixer = postcss([ autoprefixer ]);
 
-fs.readFile('./css/style.css', (err, data) => {
+// 读取整个css目录下的文件
+var cssPath = `${__dirname}/css`;
+fs.readdir(cssPath, (err, filenames) => {
 
 	if (err) throw err;
+	async.each(filenames, (file) => {
 
-	let cssStr = data.toString();
+		fs.readFile(`${cssPath}/${file}`, (err, data) => {
+			console.log(data.toString());
+		});
 
-	cleaner.process(cssStr).then(function (cleaned) {
-	    return prefixer.process(cleaned.css)
-	}).then(function (result) {
-	    savePrefixerCss(result.css);
 	});
 
 });
 
-function savePrefixerCss(css) {
-	fs.writeFile('./dest/style.css', css, (err) => {
-		if (err) throw err;
-		console.log('file write successed');
-	});
-}
+// fs.readFile('./css/style.css', (err, data) => {
+
+// 	if (err) throw err;
+
+// 	let cssStr = data.toString();
+
+// 	cleaner.process(cssStr).then(function (cleaned) {
+// 	    return prefixer.process(cleaned.css)
+// 	}).then(function (result) {
+// 	    savePrefixerCss(result.css);
+// 	});
+
+// });
+
+// function savePrefixerCss(css) {
+// 	fs.writeFile('./dest/style.css', css, (err) => {
+// 		if (err) throw err;
+// 		console.log('file write successed');
+// 	});
+// }
